9732879cb3d98fb2664c32c95ba86ac3a77b9138
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-05 22:04:12 +0800

    Create lq2_snippet.js

---
+++ lq2_snippet.js
@@ -0,0 +1,6 @@
+//declare a var that will hold the uname, use a user input method
+let uname = prompt("Register your Username");
+//declare a var that will hold the pword, use a user input method
+let pword = prompt("Register your passcode");
+//declare a variable that will hold the re-pword, use a user input method
+let rePword = prompt("Re-enter your passcode");
