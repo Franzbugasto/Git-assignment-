9522e00df37a099891a3a04819da711f1340afcc
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-05 21:59:57 +0800

    Create lq2_loops3.js

---
+++ lq2_loops3.js
@@ -0,0 +1,10 @@
+//while loop solution 
+let fName = "Jose Cruz";
+let i = 0;
+while(fName.length > i) {
+    let c = 2 + i;
+    let nLengthCounterTwo = fName.length - c;
+    console.log(fName);
+    console.log("Iterations Name was Printed: " + i);
+    i++;
+}
